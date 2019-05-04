a69cad38527bb32f8901f3f365b27b4371673edc
seedoffd <seedoffd@gmail.com>
2019-05-04 16:23:02 -0500

    parameter added

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,12 +1,14 @@
 node{
-properties([parameters([string(defaultValue: 'IP', description: 'where to build e.g IP', name: 'ENV', trim: true)])])    stage("Clone repo"{
-        git "git@github.com:seedoffd/Flaskex.git"
+    properties([parameters([string(defaultValue: 'IP', description: 'where to build e.g IP', name: 'ENV', trim: true)])])
+    stage("clone repo"){
+        git 'git@github.com:daudmu21/stormpath-flask-sample.git'
     }
-    stage("build application"){
+        stage("Build application"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
+        sh "ssh ec2-user@${ENV} sudo yum install python-pip -y"
         sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
     }
     stage("App Run"){
-        sh "ssh ec2-user@${ENV} python /tmp/app.py"
+        sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
     }
 }
